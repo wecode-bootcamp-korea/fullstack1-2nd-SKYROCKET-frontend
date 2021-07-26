1ae58f02a195872b9e81996f0622ac59cbef2ebf
Hayoung Gam <hahhyoung@gmail.com>
2021-07-26 18:08:51 +0900

    Remove: 불필요한 코드 삭제

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
-import { flex } from '../../styles/mixins';
 
 function Main() {
-  return (
-    <Container justify="space-between" align="center">
-      <Text>메인 페이지입니다!</Text>
-    </Container>
-  );
+  return <Text>메인 페이지입니다!</Text>;
 }
 
-const Container = styled.div`
-  ${flex}
-`;
-
-const Text = styled.p`
-  color: ${({ theme }) => theme.colors.black};
-`;
+const Text = styled.p``;
 
 export default Main;
