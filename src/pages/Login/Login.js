866b87b8466f5c589b785201a3e6ee650b1e80bc
Hayoung Gam <hahhyoung@gmail.com>
2021-07-28 13:49:00 +0900

    Add: 공용 Container 컴포넌트 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import Container from '../../components/Container/Container';
+import { flex } from '../../styles/mixins';
 
 function Login() {
-  return <Text>로그인 페이지입니다!</Text>;
+  return (
+    <LoginContainer justify="center" align="center">
+      <Text>로그인 페이지입니다!</Text>
+    </LoginContainer>
+  );
 }
+
+const LoginContainer = styled(Container)`
+  ${flex}
+  padding-top: 135px;
+`;
 
 const Text = styled.p``;
 
